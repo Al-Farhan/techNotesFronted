848f211acfa75d9069ecfc5c6fac6231a989c8bd
Farhan Shaikh <95585017+Al-Farhan@users.noreply.github.com>
2024-05-24 18:48:45 +0530

    start: react forms

---
+++ src/features/users/NewUserForm.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react"
+import { useAddNewUserMutation } from "./usersApiSlice"
+import { useNavigate } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSave } from "@fortawesome/free-solid-svg-icons"
+import { ROLES } from "../../config/roles"
+
+const NewUserForm = () => {
+  return (
+    <div>NewUserForm</div>
+  )
+}
+
+export default NewUserForm
