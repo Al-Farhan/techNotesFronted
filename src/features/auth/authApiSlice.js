2a183cf9e5ac72bb02a835df9c9900128ce042db
Farhan Shaikh <95585017+Al-Farhan@users.noreply.github.com>
2024-05-27 18:39:12 +0530

    login authentication first

---
+++ src/features/auth/authApiSlice.js
@@ -0,0 +1,23 @@
+import { apiSlice } from "../../app/api/apiSlice";
+import { logOut } from "./authSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        login: builder.mutation({
+            query: credentials => ({
+                url: '/auth',
+                method: 'POST',
+                body: { ...credentials }
+            })
+        }),
+        sendLogout: builder.mutation({
+            query: () => ({
+                url: '/auth/logout',
+                method: 'POST',
+            }),
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                
+            }
+        })
+    })
+})
