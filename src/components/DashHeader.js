193ee77862d7f587fd5cd988b99a691a01cf0248
Farhan Shaikh <95585017+Al-Farhan@users.noreply.github.com>
2024-06-05 16:17:18 +0530

    fix:refactor

--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -30,8 +30,6 @@ const DashHeader = () => {
         error
     }] = useSendLogoutMutation()
 
-    console.log(isSuccess);
-
     useEffect(() => {
         if (isSuccess) navigate('/')
     }, [isSuccess, navigate])
